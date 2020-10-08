6522cf3ff9f82e542ba4978736acb840aebe51fc
Thainá Ramos <51253252+rthai01@users.noreply.github.com>
2020-10-08 10:28:42 -0300

    Create server.js

---
+++ src/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const routes = require('./routes')
+
+const server = express()
+
+mongoose.connect(
+  "mongodb+srv://unifeso:unifeso-password@unifeso.kwuxv.gcp.mongodb.net/unifeso-financial-control?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  }
+);
+
+server.use(express.json())
+server.use(routes)
+
+server.listen(3333, () => console.log(`application started in http://localhost:3333`))
